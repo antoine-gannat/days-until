37dddc061f8ee4dcdd311d2e4e07ec37c9081f17
antoine-gannat <antoine.gannat@gmail.com>
2020-02-12 07:37:49 +0000

    Deploying to gh-pages from  - eea9b17ecd40a9dc8028fc73e7a292daa3f7e286 🚀

---
+++ precache-manifest.c5501a6d161d60f9947e8e30c1cd69b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d43f19132ecadbd559203f22277c2d3",
+    "url": "/deadline-manager/index.html"
+  },
+  {
+    "revision": "1537d45c0059f989df29",
+    "url": "/deadline-manager/static/css/2.53280342.chunk.css"
+  },
+  {
+    "revision": "3338fc9accdd752a0ab0",
+    "url": "/deadline-manager/static/css/main.c36a909b.chunk.css"
+  },
+  {
+    "revision": "1537d45c0059f989df29",
+    "url": "/deadline-manager/static/js/2.9c456439.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/deadline-manager/static/js/2.9c456439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3338fc9accdd752a0ab0",
+    "url": "/deadline-manager/static/js/main.58ae95a5.chunk.js"
+  },
+  {
+    "revision": "1df2fe1c8cc6500981ca",
+    "url": "/deadline-manager/static/js/runtime-main.f8a76301.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a0e4715a6ae1cdf85c20f6364913461e.js"
+  "/deadline-manager/precache-manifest.c5501a6d161d60f9947e8e30c1cd69b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deadline-manager/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
